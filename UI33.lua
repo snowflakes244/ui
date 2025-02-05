60b828576c2cd207fa84b0ddd928dff15f904bbc
snowflakes244 <2328114424@qq.com>
2025-02-06 00:00:13 +0800

    Update UI33.lua

--- UI33.lua
+++ UI33.lua
@@ -1682,8 +1682,9 @@ local UITab3 = window:Tab("功能",'')
 local UITab9 = window:Tab("功能",'')
 local UITab2 = window:Tab("功能",'') 
 
-local about2 = UITab1:section("功能", false)
+local about2 = UITab1:section("飞行功能", false)
 
-about2:Button("功能", "", false, function(state)
+about2:Button("飞行", "", false, function(state)
 loadstring(game:HttpGet("https://pastebin.com/raw/J9PbZFVP", true))()
+		
 end)
